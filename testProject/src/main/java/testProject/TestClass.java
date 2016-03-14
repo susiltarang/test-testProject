15e66fdfe21e77bed64f7bd2433b76502fa80289
susiltarang <susilr@tarangtech.com>
2016-03-14 15:13:52 +0530

    added

---
+++ testProject/src/main/java/testProject/TestClass.java
@@ -0,0 +1,21 @@
+package testProject;
+
+public class TestClass {
+
+	public static void main(String arg[]){
+		
+		main("hello");
+		
+		
+	}
+	
+	
+	
+	public static void main(String a){
+		
+		
+		
+		
+	}
+	
+}
